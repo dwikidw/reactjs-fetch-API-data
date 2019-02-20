06e69ef4898e2e54bed373b9e29b846e6f46d77f
Dwiki Baskoro <dwikibaskoro888@gmail.com>
2019-02-20 20:17:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Row, Col } from 'react-simple-flex-grid';
+import 'react-simple-flex-grid/lib/main.css';
+// import logo from './logo.svg';
 import './App.css';
+import { list } from 'postcss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    };
+  }
+  componentDidMount() {
+    fetch('https://dev.pestadiskon.com/api/Discounts')
+      .then(res => res.json())
+      .then(json => {
+        console.log('data', json);
+
+        this.setState({
+          isLoaded: true,
+          items: json,
+        });
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading....</div>;
+    } else {
+      return (
+        <div className="body">
+          {items.map(item => (
+            <Row
+              key={item.id}
+              className="card"
+              style={{ backgroundColor: item.provider === 'ovo' ? 'lightyellow' : 'lightblue' }}
+            >
+              <Row className="title">{item.shop_name}</Row>
+              <Row className="sub-title">{item.short_description}</Row>
+              <Row className="text" style={{ color: item.provider === 'ovo' ? 'purple' : 'black' }}>
+                {item.provider}
+              </Row>
+            </Row>
+          ))}
+        </div>
+      );
+    }
   }
 }
 
